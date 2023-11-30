d90303eeeff06133c7c9bf3cae573f8defd6c0be
FAhmed1I <110400203+FAhmed1I@users.noreply.github.com>
2023-11-29 22:47:57 -0330

    Add files via upload

--- balance.py
+++ balance.py
@@ -80,7 +80,8 @@ def blenca():
         problems = Text(balanceprobs, size=30, text=f'The following issues were found:\n {issues}')
         blank2 = Text(balanceprobs, size=40)
         with open('resultats.txt', 'a') as r:
-            r.write(issues)
+            for i in range(len(issues)):
+                r.write(issues[i])
         resexit = Text(balanceprobs, size=30, text='Close this window to play again')
 
 
@@ -91,10 +92,10 @@ def blenca():
 
     inste1 = Text(blnce, '1. Hold the Arduino on either side.',grid=[0,1], align='left', size=18)
     inste2 = Text(blnce, '2. Keep it as stable as you can.',grid=[0,2], align='left', size=18)
-    inste3 = Text(blnce, '3. For 30 seconds, the Arduino will note all movement', grid=[0,3], align='left', size=18)
+    inste3 = Text(blnce, '3. For ~30 seconds, the Arduino will note all movement', grid=[0,3], align='left', size=18)
     inste4 = Text(blnce, '4. The data collected will be processed', grid=[0,4], align='left', size=18)
     inste5 = Text(blnce, '5. A new window will appear on your computer, along with any issues that were found',grid=[0,5], align='left', size=18)
-    inste6 = Text(blnce, 'Press the Balance to Start', grid=[0,6], size=24)
+    inste6 = Text(blnce, 'Press the Balance twice to Start', grid=[0,6], size=24)
             
             
 blenca()
